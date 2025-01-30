2e89719786f7e8c48f783ecec30486fc3c2b77bc
miguelestradam36 <miguelestradam36@gmail.com>
2025-01-29 18:27:22 -0600

    Uploading basic c++ project

--- Laboratorio_11.cpp
+++ Laboratorio_11.cpp
@@ -1,7 +1,81 @@
 #include <iostream>
 #include <vector>
 #include <queue>
-#include "Graph.h"
+using namespace std;
+
+// Estructura de datos para almacenar un borde de graph
+struct Edge {
+    int src, dest;
+};
+
+// Una clase para representar un objeto graph
+class Graph
+{
+public:
+    // numero de vertices
+    int V;
+    // un vector de vectores para representar una lista de adyacencia
+    vector<vector<int>> adjList;
+
+    // Constructor de graph
+    Graph(vector<Edge> const& edges, int n)
+    {
+        // guardando numero de vertices
+        this->V = n;
+        // cambiar el tamaï¿½o del vector para contener `n` elementos de tipo `vector<int>`
+        adjList.resize(n);
+
+        // agrega bordes al grafo no dirigido
+        for (auto& edge : edges)
+        {
+            adjList[edge.src].push_back(edge.dest);
+            adjList[edge.dest].push_back(edge.src);
+        }
+    }
+    //Imprime grafo
+    void print_graph() {
+        for (int i = 0; i < V; i++) {
+            std::cout << "Grafo-> el vertice: " << i << " esta conectado con: ";
+            for (int j : this->adjList[i]) {
+                std::cout << " " << j;
+            }
+            std::cout << endl;
+        }
+    }
+    // Realiza DFS en el graph y devuelve verdadero si se encuentra algï¿½n borde posterior en el graph
+    bool DFS(int v, vector<bool>& discovered, int parent)
+    {
+        // marca el nodo actual como descubierto
+        discovered[v] = true;
+
+        // hacer para cada borde (v, w)
+        for (int w : this->adjList[v])
+        {
+            // si `w` no se descubre
+            if (!discovered[w])
+            {
+                if (DFS(w, discovered, v)) {
+                    return true;
+                }
+            }
+
+            // si se descubre `w` y `w` no es un padre
+            else if (w != parent)
+            {
+                // encontramos un back-edge (ciclo)
+                return true;
+            }
+        }
+
+        // No se encontraron bordes posteriores en el graph
+        return false;
+    }
+};
+
+
+#include <iostream>
+#include <vector>
+#include <queue>
 int main() {
     int accion;
     do {
@@ -27,17 +101,17 @@ int main() {
                 {0, 1}, {1, 2}, {2, 3}, {3, 4}, {4, 5}
             };
 
-            // número total de nodos en el graph (0 a 5)
+            // nï¿½mero total de nodos en el graph (0 a 5)
             int n = 6;
 
             // construye un graph a partir de los bordes dados
             Graph graph(edges, n);
 
-            // para realizar un seguimiento de si se descubre un vértice o no
+            // para realizar un seguimiento de si se descubre un vï¿½rtice o no
             vector<bool> discovered(n);
             //mostrar grafo
             graph.print_graph();
-            // Realizar recorrido DFS desde el primer vértice
+            // Realizar recorrido DFS desde el primer vï¿½rtice
             if (graph.DFS(0, discovered, -1)) {
                 std::cout << "Este grafo contiene un ciclo";
             }
@@ -54,23 +128,23 @@ int main() {
             {
                 {0, 1}, {0, 6}, {0, 7},
                 {1, 2}, {1, 5},
-                {2, 3}, {2, 4}, 
-                {7, 8}, {7, 11}, 
-                {8, 9}, {8, 10}, 
+                {2, 3}, {2, 4},
+                {7, 8}, {7, 11},
+                {8, 9}, {8, 10},
                 {10, 11}
             };
 
-            // número total de nodos en el graph (0 a 11)
+            // nï¿½mero total de nodos en el graph (0 a 11)
             int n = 12;
 
             // construye un graph a partir de los bordes dados
             Graph graph(edges, n);
 
-            // para realizar un seguimiento de si se descubre un vértice o no
+            // para realizar un seguimiento de si se descubre un vï¿½rtice o no
             vector<bool> discovered(n);
             //mostrar grafo
             graph.print_graph();
-            // Realizar recorrido DFS desde el primer vértice
+            // Realizar recorrido DFS desde el primer vï¿½rtice
             if (graph.DFS(0, discovered, -1)) {
                 cout << "Este grafo contiene un ciclo";
             }
--- README.md
+++ README.md
@@ -4,3 +4,7 @@ Univerisity quiz about directed graphs and how the can be used in data analysis.
 You fill find the data for the directed graph inside the C++ code, you may change it if you want to use another data set.
 
 This is a simple university quiz I want to save on Github.
+
+### Important
+
+The graph used in this exercise is inside the code. (If you want to modify it, please open the C++ file.)
